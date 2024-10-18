24583c9bc3f076b6778aec389121510428e8385f
Alejandro <alejossena12@gmail.com>
2024-10-17 22:58:41 -0500

    feature: * db.py added to connect bd from py client * someQueries.sql added to save all sql query

---
+++ sql/someQueries.sql
@@ -0,0 +1,16 @@
+CREATE TABLE db_users;
+USE db_users;
+
+CREATE TABLE users(
+    id INT NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20),
+    birthday DATE
+)
+
+ALTER TABLE users;
+ADD PRIMARY KEY (id);
+
+SELECT * FROM users
+
+INSERT INTO user(id, name, last_name, birthday) VALUES (456, 'CARLOS', 'ACEVEDO', '1998-29-08');
